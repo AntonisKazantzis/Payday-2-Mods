40ae9faa5dcd705003c07e18bda8d4598dde7186
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-06-06 16:34:56 +0300

    feat: add complete all side jobs of safehouse

---
+++ Complete All Side Jobs/challengemanager.lua
@@ -0,0 +1,13 @@
+local original_function = original_function or ChallengeManager.activate_challenge
+function ChallengeManager:activate_challenge(id, key, category)
+	if self:has_active_challenges(id, key) then
+		local challenge = self:get_challenge(id, key)
+		challenge.completed = true
+		challenge.rewarded = true
+		challenge.category = category
+		self._global.active_challenges[key or Idstring(id):key()] = challenge
+		
+		return true
+	end
+	return original_function(self, id, key, category)
+end
---
+++ Complete All Side Jobs/customsafehousemanager.lua
@@ -0,0 +1,43 @@
+-- Disable raids
+function CustomSafehouseManager:is_being_raided()
+	return false
+end
+
+-- Complete safe house daily
+function CustomSafehouseManager:set_active_daily(id)
+	if self:get_daily_challenge() and self:get_daily_challenge().id ~= id then
+		self:generate_daily(id)
+	end
+	
+	self:complete_and_reward_daily(id)
+end
+
+-- Max rooms tier
+local M_safehouse = managers.custom_safehouse
+local G_safehouse = Global.custom_safehouse_manager
+
+for room_id, data in pairs(G_safehouse.rooms) do
+	local max_tier = data.tier_max
+	local current_tier = M_safehouse:get_room_current_tier(room_id)
+	
+	while max_tier > current_tier do
+		current_tier = current_tier + 1
+		
+		local unlocked_tiers = M_safehouse._global.rooms[room_id].unlocked_tiers
+		tab_insert(unlocked_tiers, current_tier)
+	end
+	
+	M_safehouse:set_room_tier(room_id, max_tier)
+end
+
+-- Complete trophies
+local M_safehouse = managers.custom_safehouse
+local trophies = M_safehouse:trophies()
+
+for _, trophy in pairs(trophies) do
+	for objective_id in pairs (trophy.objectives) do
+		local objective = trophy.objectives[objective_id]
+		objective.verify = false
+		M_safehouse:on_achievement_progressed(objective.progress_id, objective.max_progress)
+	end
+end
