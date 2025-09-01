d450598d76f848b19d069bdda00d650b02b3e581
Antonis Kazantzis <kazantzisantonis@gmail.com>
2025-09-01 10:41:53 +0300

    refactor: remove required script

--- Camera Disabled Message/code.lua
+++ Camera Disabled Message/code.lua
@@ -1,20 +1,18 @@
-if RequiredScript == "lib/units/props/securitycamera" then
-    local original_set_detection_enabled = SecurityCamera.set_detection_enabled
+local original_set_detection_enabled = SecurityCamera.set_detection_enabled
 
-    local cameras_disabled = false
+local cameras_disabled = false
 
-    local function send_camera_disabled_message()
-        if not cameras_disabled then
-            managers.chat:send_message(ChatManager.GAME, managers.network.account:username() or "System", "Security cameras are disabled!")
-            cameras_disabled = true
-        end
+local function send_camera_disabled_message()
+    if not cameras_disabled then
+        managers.chat:send_message(ChatManager.GAME, managers.network.account:username() or "System", "Security cameras are disabled!")
+        cameras_disabled = true
+    end
+end
+
+function SecurityCamera:set_detection_enabled(state, settings, mission_element, ...)
+    if not state then
+        send_camera_disabled_message()
     end
 
-    function SecurityCamera:set_detection_enabled(state, settings, mission_element, ...)
-        if not state then
-            send_camera_disabled_message()
-        end
-
-        return original_set_detection_enabled(self, state, settings, mission_element, ...)
-    end
+    return original_set_detection_enabled(self, state, settings, mission_element, ...)
 end
