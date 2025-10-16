8a7a36a80e148aec57ce0b24f0a10e4952edd5c8
Antonis Kazantzis <kazantzisantonis@gmail.com>
2025-10-16 19:29:42 +0300

    feat: fix error

--- Warp To Crosshair/code.lua
+++ Warp To Crosshair/code.lua
@@ -1,6 +1,10 @@
 local PENETRATE = true
 local player_unit = managers.player:player_unit()
+if not player_unit then return end
+
 local camera = player_unit:camera()
+if not camera then return end
+
 local mvec_to = Vector3()
 local from_pos = camera:position()
 
@@ -10,13 +14,18 @@ mvector3.add(mvec_to, from_pos)
 
 local col_ray = World:raycast("ray", from_pos, mvec_to, "slot_mask", managers.slot:get_mask("bullet_impact_targets"))
 
-if col_ray then
+if col_ray and col_ray.hit_position then
+    -- manually copy hit_position into a new vector
+    local warp_pos = Vector3(col_ray.hit_position.x, col_ray.hit_position.y, col_ray.hit_position.z)
+
     if PENETRATE then
         local offset = Vector3()
         mvector3.set(offset, camera:forward())
         mvector3.multiply(offset, 100)
-        mvector3.add(col_ray.hit_position, offset)
+        mvector3.add(warp_pos, offset)
     end
 
-    managers.player:warp_to(col_ray.hit_position, player_unit)
+    -- Use camera rotation as rot parameter
+    local rot = camera:rotation()
+    managers.player:warp_to(warp_pos, rot)
 end
